843d68dfa3d7eb22ad3728eff0a3a5892b7da55c
phantomedc <gaolushen123@gmail.com>
2019-04-13 10:13:37 +0800

    remove file template

--- src/leetcode/characterchallenge/CharacterMultiply.java
+++ src/leetcode/characterchallenge/CharacterMultiply.java
@@ -1,19 +1,6 @@
-/*
- * Copyright © 2013-2019 Suning.com Co., Ltd.
- *
- * This file is part of leetcode-bytedance project.
- * You can not be copied and/or distributed without the express
- * permission of cloudytrace group.
- */
 package leetcode.characterchallenge;
 
-/**
- * leetcode-bytedance: CharacterMultiply
- *
- * @author 何硕 (Employee ID: 18032496)
- * @version 1.0.0, 2019-04-12 09:47
- * @since 1.0.0, 2019-04-12 09:47
- */
+
 public class CharacterMultiply {
 
     public static void main(String[] args) {
