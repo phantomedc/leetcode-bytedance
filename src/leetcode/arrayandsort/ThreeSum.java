7d69aa33d293b55bbf6ecc405608310785bb1586
phantomedc <gaolushen123@126.com>
2019-04-15 09:45:34 +0800

    ReverseList

--- src/leetcode/arrayandsort/ThreeSum.java
+++ src/leetcode/arrayandsort/ThreeSum.java
@@ -1,18 +1,14 @@
-/*
- * Copyright © 2013-2019 Suning.com Co., Ltd.
- *
- * This file is part of leetcode-bytedance project.
- * You can not be copied and/or distributed without the express
- * permission of cloudytrace group.
- */
 package leetcode.arrayandsort;
 
-/**
- * leetcode-bytedance: ThreeSum
- *
- * @author 何硕 (Employee ID: 18032496)
- * @version 1.0.0, 2019-04-12 16:55
- * @since 1.0.0, 2019-04-12 16:55
- */
+import java.util.List;
+
 public class ThreeSum {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        return null;
+    }
 }
--- src/leetcode/linkedlistandtree/ReverseList.java
+++ src/leetcode/linkedlistandtree/ReverseList.java
@@ -1,19 +1,5 @@
-/*
- * Copyright © 2013-2019 Suning.com Co., Ltd.
- *
- * This file is part of leetcode-bytedance project.
- * You can not be copied and/or distributed without the express
- * permission of cloudytrace group.
- */
 package leetcode.linkedlistandtree;
 
-/**
- * leetcode-bytedance: ReverseList
- *
- * @author 何硕 (Employee ID: 18032496)
- * @version 1.0.0, 2019-04-15 08:41
- * @since 1.0.0, 2019-04-15 08:41
- */
 public class ReverseList {
 
     public static class ListNode {
